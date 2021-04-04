b4abcfd37475eddc03837de67a48722197739434
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2021-04-04 12:27:14 +0100

    Components generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { BannerImageComponent } from './components/banner-image/banner-image.component';
+import { PopupComponent } from './components/popup/popup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    MainContentComponent,
+    BannerImageComponent,
+    PopupComponent
   ],
   imports: [
     BrowserModule
