90c4a9506453fed4e420ddfae451885f8ee35d3e
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2021-04-04 13:08:09 +0100

    Basic image data setup

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -9,6 +9,46 @@ export class MainContentComponent implements OnInit {
 
   constructor() { }
 
+  primaryMainContentImage = {
+    image: '../../../assets/images/enhance-your-home-outdoor-furniture.jpg',
+    text: 'Outdoor Furniture'
+  }
+
+  imageSetOne = [
+    {
+      image: '../../../assets/images/enhance-your-home-bathroom.jpg',
+      text: 'Bathroom Accessories'
+    },
+    {
+      image: '../../../assets/images/enhance-your-home-storage.jpg',
+      text: 'Storage Solutions'
+    },
+    {
+      image: '../../../assets/images/enhance-your-home-outdoor.jpg',
+      text: 'Outdoor Living'
+    },
+    {
+      image: '../../../assets/images/enhance-your-home-garden.jpg',
+      text: 'Garden Ornaments'
+    }
+  ];
+
+  secondaryMainContentImage = {
+    image: '../../../assets/images/enhance-your-home-new.jpg',
+    text: 'New In'
+  };
+
+  imageSetTwo = [
+    {
+      image: '../../../assets/images/enchance-your-home-dining.jpg',
+      text: 'Dining Chairs'
+    },
+    {
+      image: '../../../assets/images/enhance-your-home-buyers-choice.jpg',
+      text: 'Buyers Choice'
+    }
+  ];
+
   ngOnInit(): void {
   }
 
